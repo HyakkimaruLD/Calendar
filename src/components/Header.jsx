f05fbf4a04f8d879e68c2e40344ba8d4f4af8905
Vlad Chernukha <forreadtr@gmail.com>
2025-05-18 22:17:29 +0300

    Дода

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,3 +14,5 @@ export default function Header({ year, month, onPrev, onNext, onToday }) {
         </div>
     )
 }
+
+
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -44,3 +44,4 @@ export default function NoteList({ notes, onEdit, onDelete }) {
         </div>
     );
 }
+
